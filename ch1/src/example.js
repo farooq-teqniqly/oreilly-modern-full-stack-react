a78f85eb77d884287e96e36ab016488593a3636b
farooqfarooq@teqniqly.com <farooq@teqniqly.com>
2024-11-03 18:56:12 -0800

    Add Post model and implement sample post creation

--- ch1/src/example.js
+++ ch1/src/example.js
@@ -1,3 +1,16 @@
 import { initDatabase } from "./db/init.js";
+import { Post } from "./db/models/post.js";
 
-initDatabase();
+await initDatabase();
+
+const post = new Post({
+  title: "Creating a full-stack app using MongoDB, Express, React, and Node.js",
+  author: "Farooq Mahmud",
+  contents: "This is a sample post.",
+  tags: ["mongoose", "node.js", "javascript"],
+});
+
+await post.save();
+
+const posts = await Post.find();
+console.log(posts);
