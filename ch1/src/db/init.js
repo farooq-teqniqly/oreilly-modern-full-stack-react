4438bcdd2ff8f216ca8f26f248296c0157b3f323
Farooq Mahmud <mahmud.farooq@acuitybrands.com>
2024-11-04 08:35:16 -0800

    Update package.json and jest.config.json

--- ch1/src/db/init.js
+++ ch1/src/db/init.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const DATABASE_URL = "mongodb://localhost:27017/blog";
+const DATABASE_URL = process.env.DATABASE_URL;
 
 export function initDatabase() {
   mongoose.connection.on("open", () => {
