34508eba02a939fc974cafb21a8a8fde559c35a6
farooqfarooq@teqniqly.com <farooq@teqniqly.com>
2024-11-05 12:37:20 -0800

    Add CORS middleware to enable cross-origin requests

--- ch1/src/app.js
+++ ch1/src/app.js
@@ -1,11 +1,13 @@
 import express from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
 import swaggerUi from "swagger-ui-express";
 import specs from "../swaggerConfig.js";
 
 import { postsRoutes } from "./routes/posts.js";
 
 const app = express();
+app.use(cors());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 app.use(bodyParser.json());
 postsRoutes(app);
