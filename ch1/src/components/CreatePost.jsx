c10573f29cc7925776e39b73f533c5fa1a45a4af
farooqfarooq@teqniqly.com <farooq@teqniqly.com>
2024-11-07 15:17:06 -0800

    Implement createPost API call and integrate with CreatePost component; add form handling and mutation for post creation

--- ch1/src/components/CreatePost.jsx
+++ ch1/src/components/CreatePost.jsx
@@ -1,19 +1,72 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useState } from "react";
+import { createPost } from "../api/posts.js";
+
 export const CreatePost = () => {
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [contents, setContents] = useState("");
+
+  const queryClient = useQueryClient();
+
+  const createPostMutation = useMutation({
+    mutationFn: () => createPost({ title, author, contents }),
+    onSuccess: () => queryClient.invalidateQueries(["posts"]),
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createPostMutation.mutate();
+  };
+
   return (
-    <form onSubmit={(e) => e.preventDefault()}>
+    <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor="create-title">Title:</label>
-        <input type="text" name="create-title" id="create-title" />
+        <input
+          type="text"
+          name="create-title"
+          id="create-title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
       </div>
       <div>
         <label htmlFor="create-author">Author:</label>
-        <input type="text" name="create-author" id="create-author" />
+        <input
+          type="text"
+          name="create-author"
+          id="create-author"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        />
       </div>
       <br />
-      <textarea></textarea>
+      <textarea
+        value={contents}
+        onChange={(e) => setContents(e.target.value)}
+      ></textarea>
       <br />
       <br />
-      <input type="submit" value="Create Post" />
+      <input
+        type="submit"
+        value={createPostMutation.isPending ? "Creating..." : "Create"}
+        disabled={
+          !title || !author || !contents || createPostMutation.isPending
+        }
+      />
+      {createPostMutation.isSuccess ? (
+        <>
+          <br />
+          Post created!
+        </>
+      ) : null}
+      {createPostMutation.isError ? (
+        <>
+          <br />
+          Could not create post!
+        </>
+      ) : null}
     </form>
   );
 };
